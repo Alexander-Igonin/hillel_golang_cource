81363360aeeb0c5d4ccd9bcb376e41d711322840
Alexander-Igonin <oleksandr.igonin@aura.com>
2024-04-15 15:41:00 +0300

    remove redundant strcucts from handlers

--- lesson_9/server/handlers/get_pc.go
+++ lesson_9/server/handlers/get_pc.go
@@ -1,19 +1,11 @@
 package handlers
 
 import (
-	computer "hillel_go_cource/lesson_9/models"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
-type GetResponcePcBody struct {
-	Pc *computer.Computer
-}
-
 func (h *Handlers) GetPcHandler(c echo.Context) error {
-	var pc GetResponcePcBody
-	pc.Pc = h.Pc
-	
-	return c.JSON(http.StatusOK, pc)
+	return c.JSON(http.StatusOK, h.Pc)
 }
--- lesson_9/server/handlers/post_new_pc.go
+++ lesson_9/server/handlers/post_new_pc.go
@@ -7,22 +7,11 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-type PostNewPcRequestBody struct {
-	Cpu string
-	Gpu string
-	Motherboard string
-	Ram int
-	Ssd int
-	PowerBlock int
-	Water bool
-}
-
-
 func (h *Handlers) PostNewPcHandler(c echo.Context) error {
-	
+
 	if err := c.Bind(&h.Pc); err != nil {
 		return fmt.Errorf("post request parse error: %w", err)
 	}
 
-	return c.JSON(http.StatusOK, &h.Pc)
+	return c.JSON(http.StatusOK, h.Pc)
 }
